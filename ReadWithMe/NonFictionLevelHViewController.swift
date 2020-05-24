20e917d12cfc0dcb159f2f367717f1b195b1cb54
mcheng8 <50517517+mcheng8@users.noreply.github.com>
2020-05-24 12:07:34 -0400

    Completed Iteration 3
    Has Scroll View on Text Level
    Has Sound Recording and Playback on Non Fiction Screen Only

--- ReadWithMe/NonFictionLevelHViewController.swift
+++ ReadWithMe/NonFictionLevelHViewController.swift
@@ -9,38 +9,129 @@
 import UIKit
 import AVFoundation
 
-class NonFictionLevelHViewController: UIViewController {
+class NonFictionLevelHViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
 
-    var audioPlayer: AVAudioPlayer?
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var listenButton: UIButton!
+    
+    var audioPlayer: AVAudioPlayer!
+    var audioRecorder: AVAudioRecorder!
+    
+    // Change the audioRecordingName for each different reading
+    var audioRecordingName : String = "NonFictionLevelH.m4a"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        recordSetup()
+        playButton.isEnabled = false
+        listenButton.isEnabled = false
+    }
 
-        // Do any additional setup after loading the view.
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
     
-    @IBAction func listenTapped(_ sender: UIButton) {
-    
-        // TAGS:
-        // #1: Listen Button
+    func recordSetup() {
+        let audioFilename = getDocumentsDirectory().appendingPathComponent(audioRecordingName)
+        let audioFormat = [AVFormatIDKey : kAudioFormatAppleLossless,
+                            AVEncoderAudioQualityKey : AVAudioQuality.max.rawValue ,
+                            AVEncoderBitRateKey : 320000,
+                            AVNumberOfChannelsKey : 2,
+                            AVSampleRateKey : 44100.2 ] as [String : Any]
         
-        let selectedRecording = sender.tag
-        
-        switch selectedRecording {
-        case 1:
-            let pathToRecording = Bundle.main.path(forResource: "HNonfictionText", ofType: "m4a")
-            let addressToFile = URL(fileURLWithPath: pathToRecording!)
-            
-            do{
-                audioPlayer = try AVAudioPlayer(contentsOf: addressToFile)
-                audioPlayer?.play()
-            } catch {
-                // error handling
-            }
-            
-        default:
-            return
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: audioFormat)
+            audioRecorder.delegate = self
+            audioRecorder.prepareToRecord()
+        } catch {
+            print(error)
         }
+    }
+    
+    func setupPlayer() {
+        let audioFilename = getDocumentsDirectory().appendingPathComponent(audioRecordingName)
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioFilename)
+            audioPlayer.delegate = self
+            audioPlayer.prepareToPlay()
+            audioPlayer.volume = 1.0
+        } catch {
+            print(error)
+        }
+    }
+    
+    func setupPlayerForListen() {
+        // Change the pathToRecording for each different reading
+        let pathToPrerecording = Bundle.main.path(forResource: "HNonfictionText", ofType: "m4a")
+        let audioFilename = URL(fileURLWithPath: pathToPrerecording!)
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioFilename)
+            audioPlayer.delegate = self
+            audioPlayer.prepareToPlay()
+            audioPlayer.volume = 1.0
+        } catch {
+            print(error)
+        }
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        recordButton.isEnabled = true
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        recordButton.isEnabled = true
+        playButton.setTitle("Play", for: .normal)
+        listenButton.setTitle("Listen", for: .normal)
+    }
+    
+    @IBAction func recordTapped(_ sender: UIButton) {
+        if recordButton.titleLabel?.text == "Record" {
+            audioRecorder.record()
+            recordButton.setTitle("Stop", for: .normal)
+            playButton.isEnabled = false
+            listenButton.isEnabled = false
+        } else {
+            audioRecorder.stop()
+            recordButton.setTitle("Record", for: .normal)
+            playButton.isEnabled = true
+            listenButton.isEnabled = true
+        }
+    }
+    
+    @IBAction func playTapped(_ sender: UIButton) {
+        if playButton.titleLabel?.text == "Play" {
+            playButton.setTitle("Stop", for: .normal)
+            recordButton.isEnabled = false
+            listenButton.isEnabled = false
+            setupPlayer()
+            audioPlayer.play()
+        } else {
+            audioPlayer.stop()
+            playButton.setTitle("Play", for: .normal)
+            recordButton.isEnabled = true
+            listenButton.isEnabled = true
+        }
+    }
+    
+    
+    @IBAction func listenTapped(_ sender: UIButton) {
+        if listenButton.titleLabel?.text == "Listen" {
+            listenButton.setTitle("Stop", for: .normal)
+            recordButton.isEnabled = false
+            playButton.isEnabled = false
+            setupPlayerForListen()
+            audioPlayer.play()
+        } else {
+            audioPlayer.stop()
+            playButton.setTitle("Play", for: .normal)
+            recordButton.isEnabled = true
+            playButton.isEnabled = true
+        }
+       
     }
     
 
